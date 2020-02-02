9f25c8c01339a8969237bbb7a5fd049fe7bf4447
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-02 22:50:41 +0100

    Add Redux Thunk

--- index.js
+++ index.js
@@ -6,15 +6,14 @@ import React from 'react';
 import { AppRegistry } from 'react-native';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { Provider as StoreProvider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+import thunk from 'redux-thunk';
 import reducer from './src/reducers/PeopleReducer';
 import App from './src/components/App';
 import { name as appName } from './app.json';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 export default function Main() {
   return (
