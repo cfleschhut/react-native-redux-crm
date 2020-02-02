f9cf462841af1f3ba2eebc9075493cf8df5d302e
Christian Fleschhut <weltrekordhalter@gmail.com>
2020-02-02 23:54:44 +0100

    Replace avatar icon w/ person initials

--- src/components/PersonCard.js
+++ src/components/PersonCard.js
@@ -2,13 +2,17 @@ import React from 'react';
 import { StyleSheet } from 'react-native';
 import { Card, Avatar, Button } from 'react-native-paper';
 
+const getInitials = ({ first_name, last_name }) => {
+  return first_name[0] + last_name[0];
+};
+
 const PersonCard = ({ person, noneSelected }) => (
   <Card style={styles.card}>
     <Card.Cover source={{ uri: 'https://picsum.photos/700' }} />
     <Card.Title
       title={`${person.first_name} ${person.last_name}`}
       subtitle={person.email}
-      left={props => <Avatar.Icon {...props} icon="folder" />}
+      left={props => <Avatar.Text {...props} label={getInitials(person)} />}
     />
 
     {noneSelected && (
